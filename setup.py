fc3c4b46a5b095cdcdfc00c316ef711351a34975
EKaxada <enocksdha@gmail.com>
2021-02-15 14:13:27 +0300

    initiated setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='rpg',
+      version='0.1',
+      description='Random Password generator',
+      url='',
+      author='Enock Kasaadha',
+      author_email='enocksdha@gmail.com',
+      license='MIT',
+      packages=['source'],
+      python_requires=">=3",
+      zip_safe=False)
